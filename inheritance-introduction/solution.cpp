90891376cf44d38df648a4142848a062bda814b4
puranikyash <yash.puranik@mitwpu.edu.in>
2025-11-06 00:10:30 +0530

    Create solution.cpp

---
+++ inheritance-introduction/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Triangle {
+    public:
+        void triangle() {
+            cout << "I am a triangle\n";
+        }
+};
+
+class Isosceles : public Triangle {
+    public:
+        void isosceles() {
+            cout << "I am an isosceles triangle\n";
+        }
+};
+
+int main() {
+    Isosceles isc;
+    isc.isosceles();
+    isc.triangle();
+    return 0;
+}
